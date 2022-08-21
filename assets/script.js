564b878b27a319762a2a7218d04cad5525853f90
brandondijon <108966627+brandondijon@users.noreply.github.com>
2022-08-20 17:23:26 -0700

    added event listener

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,34 @@
 // global variables
-var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '~', '`', '<', '>', '?', '/', '[', ']'];
+var specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "~",
+  "`",
+  "<",
+  ">",
+  "?",
+  "/",
+  "[",
+  "]",
+];
+
+var generate = document.querySelector('#generate');
+
+
+// event listener when generate button  is clicked
+var generateBtn = document.querySelector('#generate');
+console.log(generateBtn);
+
+
