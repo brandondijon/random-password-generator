980062a7a26f7a86c058eaf0e623cc966b8b4a3d
brandondijon <108966627+brandondijon@users.noreply.github.com>
2022-08-20 18:00:23 -0700

    generate button  welcome message addded with prompt

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,22 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword()
+
+    // password ngenerated and alert pops up//
+
+  // alert welcome to password generator//
+
+    var popupTest = prompt('welcome lets generate!')
+    console.log(popupTest);
+    // Assignment Code
+    var welcomeMessage = confirm('Welcome to the password generator! Get ready to generate your  password?')
+    console.log(welcomeMessage);
+    confirm('Welcome to the Password Generator! Ready to Generate Your Password?');
+    //user says "ok"//
+    if (welcomeMessage===true){
+            confirm('Lets go!!')
+    }
